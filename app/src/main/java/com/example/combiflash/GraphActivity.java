2867e55080cf9f1b7879566e9c6da1d8ffedcca0
hd-agarwal <harshdhar2000@gmail.com>
2021-12-16 23:33:00 +0530

    added spinner to select equation to draw

--- app/src/main/java/com/example/combiflash/GraphActivity.java
+++ app/src/main/java/com/example/combiflash/GraphActivity.java
@@ -4,19 +4,18 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.github.mikephil.charting.charts.CombinedChart;
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.components.Legend;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.components.YAxis;
-import com.github.mikephil.charting.data.BarData;
-import com.github.mikephil.charting.data.BarDataSet;
-import com.github.mikephil.charting.data.BarEntry;
-import com.github.mikephil.charting.data.CombinedData;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
@@ -26,43 +25,42 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class GraphActivity extends AppCompatActivity {
-//    TextView tvTest;
     String mode;
     SampleData[] sampleData = new SampleData[2];
     float[][] cvValues=new float[2][];
     private LineChart chart;
+    Spinner spinner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_graph);
+        spinner=findViewById(R.id.spGraph);
         chart = findViewById(R.id.chart);
-        chart.getDescription().setEnabled(false);
-        chart.setBackgroundColor(Color.WHITE);
-        chart.setDrawGridBackground(false);
+        getDataFromIntent();
+        setupChart();
+        setUpSpinner();
+    }
 
+    private void getDataFromIntent() {
         Intent intent = getIntent();
         mode = intent.getStringExtra(getResources().getString(R.string.modeKey));
         if (mode.equals(getResources().getString(R.string.modeDual))) {
             sampleData[0] = new SampleData(intent.getFloatExtra(getResources().getString(R.string.concentration1Key), -1.0f), intent.getFloatArrayExtra(getResources().getString(R.string.rfArray1Key)));
             sampleData[1] = new SampleData(intent.getFloatExtra(getResources().getString(R.string.concentration2Key), -1.0f), intent.getFloatArrayExtra(getResources().getString(R.string.rfArray2Key)));
-//            tvTest.setText("Mode: " + mode + "\n" + sampleData[0].toString() + sampleData[1].toString());
             cvValues[0]=sampleData[0].getSortedCvValues();
             cvValues[1]=sampleData[1].getSortedCvValues();
 
         } else if (mode.equals(getResources().getString(R.string.modeStandard))) {
             sampleData[0] = new SampleData(intent.getFloatExtra(getResources().getString(R.string.concentrationKey), -1.0f), intent.getFloatArrayExtra(getResources().getString(R.string.rfArrayKey)));
             cvValues[0]=sampleData[0].getSortedCvValues();
-            Log.e("TAG", "onCreate: "+ Arrays.toString(sampleData[0].getRfValues()));
-            Log.e("TAG", "onCreate: "+ Arrays.toString(cvValues[0]));
-            drawGraphStandard();
-
-            //            tvTest.setText("Mode: " + mode + "\n" + sampleData[0].toString());
         }
-
-
     }
 
-    private void drawGraphStandard() {
+    private void setupChart() {
+        chart.getDescription().setEnabled(false);
+        chart.setBackgroundColor(Color.WHITE);
+        chart.setDrawGridBackground(false);
+        chart.setTouchEnabled(true);
         Legend l = chart.getLegend();
         l.setWordWrapEnabled(true);
         l.setVerticalAlignment(Legend.LegendVerticalAlignment.BOTTOM);
@@ -72,54 +70,110 @@ public class GraphActivity extends AppCompatActivity {
 
         YAxis rightAxis = chart.getAxisRight();
         rightAxis.setDrawGridLines(false);
-        rightAxis.setAxisMinimum(0f); // this replaces setStartAtZero(true)
+        rightAxis.setAxisMinimum(0f);
 
         YAxis leftAxis = chart.getAxisLeft();
         leftAxis.setDrawGridLines(true);
-        leftAxis.setAxisMinimum(0f); // this replaces setStartAtZero(true)
+        leftAxis.setAxisMinimum(0f);
 
         XAxis xAxis = chart.getXAxis();
         xAxis.setPosition(XAxis.XAxisPosition.BOTH_SIDED);
         xAxis.setAxisMinimum(0f);
         xAxis.setGranularity(1f);
         xAxis.setDrawGridLines(false);
-//        xAxis.setValueFormatter(new ValueFormatter() {
-//            @Override
-//            public String getFormattedValue(float value) {
-//            }
-//        });
+    }
 
-        ArrayList<ILineDataSet> data=new ArrayList<>();
-        data.add(generateLineDataStandard());
-        data.addAll(generateVerticalLineDataStandard());
-//        xAxis.setAxisMaximum(Math.max(data.get(0).getXMax(),data.get(1).getXMax()) + 0.25f);
-        xAxis.setAxisMaximum(cvValues[0][cvValues[0].length-1]+cvValues[0][0]);
-        LineData lineData=new LineData(data);
-        chart.setTouchEnabled(true);
-        chart.setData(lineData);
-        chart.invalidate();
+    private void setUpSpinner() {
+        ArrayAdapter<CharSequence> adapter=ArrayAdapter.createFromResource(this,R.array.graphTypes, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Log.e("TAG", "onItemSelected: "+position+" "+id );
+                ArrayList<ILineDataSet> data=new ArrayList<>();
+                if (mode.equals(getResources().getString(R.string.modeDual))) {
+                    switch (position){
+                        case 0:
+                            data.add(generateLineDataDualEqn1());
+                            break;
+                        case 1:
+                            data.add(generateLineDataDualEqn2());
+                            break;
+                        case 2:
+                            data.add(generateLineDataDualEqn3());
+                            break;
+                    }
+                } else if (mode.equals(getResources().getString(R.string.modeStandard))) {
+                    switch (position){
+                        case 0:
+                            data.add(generateLineDataStandardEqn1());
+                            break;
+                        case 1:
+                            data.add(generateLineDataStandardEqn2());
+                            break;
+                        case 2:
+                            data.add(generateLineDataStandardEqn3());
+                            break;
+                    }
+                    data.addAll(generateVerticalLineDataStandard());
+                    chart.getXAxis().setAxisMaximum(cvValues[0][cvValues[0].length-1]+cvValues[0][0]);
+                }
+                LineData lineData=new LineData(data);
+                chart.setData(lineData);
+                chart.invalidate();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+    }
+
+    private ILineDataSet generateLineDataDualEqn3() {
+        ArrayList<Entry> entries = new ArrayList<>();
+        LineDataSet set = new LineDataSet(entries, "Line DataSet 2");
+        return set;
+    }
+
+    private ILineDataSet generateLineDataDualEqn2() {
+        ArrayList<Entry> entries = new ArrayList<>();
+        LineDataSet set = new LineDataSet(entries, "Line DataSet 2");
+        return set;
+    }
+
+    private ILineDataSet generateLineDataDualEqn1() {
+        ArrayList<Entry> entries = new ArrayList<>();
+        LineDataSet set = new LineDataSet(entries, "Line DataSet 2");
+        return set;
     }
 
     @NonNull
-    private LineDataSet generateLineDataStandard() {
-
-//        LineData d = new LineData();
-
+    private LineDataSet generateLineDataStandardEqn3() {
         ArrayList<Entry> entries = new ArrayList<>();
         Log.e("TAG", "generateLineDataStandard: "+ Arrays.toString(cvValues[0]));
-
         entries.add(new Entry(cvValues[0][0],sampleData[0].concentration/4));
         entries.add(new Entry(cvValues[0][0]+(cvValues[0][cvValues[0].length-1]-cvValues[0][0])/13,sampleData[0].concentration/4));
-        entries.add(new Entry(cvValues[0][0]+10*(cvValues[0][cvValues[0].length-1]-cvValues[0][0])/13,sampleData[0].concentration*2));
-        entries.add(new Entry(cvValues[0][cvValues[0].length-1],sampleData[0].concentration*2));
-        LineDataSet set = new LineDataSet(entries, "Line DataSet");
+        entries.add(new Entry(cvValues[0][0]+10*(cvValues[0][cvValues[0].length-1]-cvValues[0][0])/13,Math.min(100,sampleData[0].concentration*2)));
+        entries.add(new Entry(cvValues[0][cvValues[0].length-1],Math.min(100,sampleData[0].concentration*2)));
+        LineDataSet set = new LineDataSet(entries, "Line DataSet 3");
         set.setColor(Color.rgb(31, 136, 222));
         set.setLineWidth(1f);
         set.setDrawCircles(false);
         set.setMode(LineDataSet.Mode.LINEAR);
         set.setDrawValues(false);
-
         set.setAxisDependency(YAxis.AxisDependency.LEFT);
+        return set;
+    }
+    private LineDataSet generateLineDataStandardEqn2(){
+        ArrayList<Entry> entries = new ArrayList<>();
+        LineDataSet set = new LineDataSet(entries, "Line DataSet 2");
+        return set;
+    }
+    private LineDataSet generateLineDataStandardEqn1(){
+        ArrayList<Entry> entries = new ArrayList<>();
+        LineDataSet set = new LineDataSet(entries, "Line DataSet 1");
         return set;
     }
     @NonNull
@@ -129,7 +183,6 @@ public class GraphActivity extends AppCompatActivity {
         for(int i=0;i<cvValues[0].length;i++)
         {
             ArrayList<Entry> entries = new ArrayList<>();
-
             entries.add(new Entry(cvValues[0][i],0f));
             entries.add(new Entry(cvValues[0][i],sampleData[0].getConcentration()*2+17f));
             LineDataSet set = new LineDataSet(entries, "Line DataSet cv");
@@ -138,7 +191,6 @@ public class GraphActivity extends AppCompatActivity {
             set.setDrawCircles(false);
             set.setMode(LineDataSet.Mode.LINEAR);
             set.setDrawValues(false);
-
             set.setAxisDependency(YAxis.AxisDependency.LEFT);
             sets.add(set);
         }
