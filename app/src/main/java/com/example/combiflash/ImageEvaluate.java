cf2db6c9983af76c2c33df590d51a9f936e1617e
hd-agarwal <harshdhar2000@gmail.com>
2021-12-30 19:07:02 +0530

    bug fix

--- app/src/main/java/com/example/combiflash/ImageEvaluate.java
+++ app/src/main/java/com/example/combiflash/ImageEvaluate.java
@@ -6,7 +6,6 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.util.Log;
-import android.util.Pair;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,7 +18,6 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 
 import java.text.DecimalFormat;
 import java.util.HashMap;
-import java.util.Objects;
 import java.util.Stack;
 
 public class ImageEvaluate extends AppCompatActivity implements View.OnTouchListener {
@@ -32,7 +30,7 @@ public class ImageEvaluate extends AppCompatActivity implements View.OnTouchList
     int lockClicked = 0, pos;
     float dy, valueMoveUp, concentration;
     Stack<View> viewStack;
-    HashMap<View, Pair<Handler, Runnable>> rfLockHandlersMap;
+    HashMap<View, Handler> rfLockHandlersMap;
     ViewGroup view;
     private static final DecimalFormat dfZero = new DecimalFormat("0.00");
 
@@ -77,6 +75,8 @@ public class ImageEvaluate extends AppCompatActivity implements View.OnTouchList
                     View v = viewStack.peek();
                     parentLayout.removeView(v);
                     viewStack.pop();
+                    clearLockTimerRunnable(v);
+                    rfLockHandlersMap.remove(v);
                 }
             }
         });
@@ -119,23 +119,15 @@ public class ImageEvaluate extends AppCompatActivity implements View.OnTouchList
             switch (motionEvent.getAction()) {
                 case MotionEvent.ACTION_DOWN:
                     dy = view.getY() - motionEvent.getRawY();
-//                    remove timer
-                    if (rfLockHandlersMap.get(view) != null) {
-                        rfLockHandlersMap.get(view).first.removeCallbacksAndMessages(null);
-                    }
+                    clearLockTimerRunnable(view);
                     break;
                 case MotionEvent.ACTION_MOVE:
                     view.setY(motionEvent.getRawY() + dy);
-//                    remove timer
-                    if (rfLockHandlersMap.get(view) != null) {
-                        rfLockHandlersMap.get(view).first.removeCallbacksAndMessages(null);
-                    }
+                    clearLockTimerRunnable(view);
                     break;
                 case MotionEvent.ACTION_UP:
-                    Handler handler = new Handler();
-                    Runnable myRunnable = () -> lockLayout(view);
-                    handler.postDelayed(myRunnable, 2000);
-                    rfLockHandlersMap.put(view, new Pair<>(handler, myRunnable));
+                    clearLockTimerRunnable(view);
+                    setLockTimerRunnable(view);
                 default:
                     return false;
             }
@@ -148,7 +140,7 @@ public class ImageEvaluate extends AppCompatActivity implements View.OnTouchList
     private void lockLayout(View layout) {
         ImageView lockBtn = layout.findViewById(R.id.lockBtn);
         TextView textHeight = layout.findViewById(R.id.height);
-        View line=layout.findViewById(R.id.viewLine);
+        View line = layout.findViewById(R.id.viewLine);
         lockClicked = 1;
         int l1[] = new int[2];
         int l2[] = new int[2];
@@ -165,61 +157,45 @@ public class ImageEvaluate extends AppCompatActivity implements View.OnTouchList
         line.setBackgroundResource(R.drawable.line_locked);
     }
 
+    private void unlockLayout(View layout) {
+        TextView textHeight = layout.findViewById(R.id.height);
+        View line = layout.findViewById(R.id.viewLine);
+        ImageView lockBtn = layout.findViewById(R.id.lockBtn);
+        line.setBackgroundResource(R.drawable.line2);
+        lockClicked = 0;
+        textHeight.setVisibility(View.INVISIBLE);
+        lockBtn.setImageDrawable(getResources().getDrawable(R.drawable.unlock));
+        layout.setEnabled(true);
+        layout.setOnTouchListener(ImageEvaluate.this);
+    }
+
+    private void clearLockTimerRunnable(View view) {
+        rfLockHandlersMap.get(view).removeCallbacksAndMessages(null);
+    }
+
+    private void setLockTimerRunnable(View view) {
+        Runnable myRunnable = () -> lockLayout(view);
+        rfLockHandlersMap.get(view).postDelayed(myRunnable, 2000);
+    }
+
     public void addViewItem() {
         View layout = getLayoutInflater().inflate(R.layout.line_layout, view, false);
         viewStack.push(layout);
+        rfLockHandlersMap.put(layout, new Handler());
         ImageView lockBtn = layout.findViewById(R.id.lockBtn);
-        TextView textHeight = layout.findViewById(R.id.height);
-        View line=layout.findViewById(R.id.viewLine);
 
         lockBtn.setImageDrawable(getResources().getDrawable(R.drawable.unlock));
 
         layout.setOnTouchListener(this);
         lockBtn.setOnClickListener(it -> {
             if (lockClicked == 0) {
-                //locked
-                lockClicked = 1;
-                int l1[] = new int[2];
-                int l2[] = new int[2];
-                bottomView.getLocationOnScreen(l1);
-                float btl = (float) l1[1] - (((float) bottomView.getHeight()) / (float) 2) - getResources().getDimensionPixelSize(R.dimen.pad);
-                layout.getLocationOnScreen(l2);
-                float k = (float) (((float) (concentration * (l2[1] - pos + getResources().getDimensionPixelSize(R.dimen.pad2)))) /
-                        ((float) (btl - pos + getResources().getDimensionPixelSize(R.dimen.pad2))));
-                float actualDistance = (concentration - k) / concentration;
-                textHeight.setText("Rf " + dfZero.format(actualDistance));
-                textHeight.setVisibility(View.VISIBLE);
-                lockBtn.setImageDrawable(getResources().getDrawable(R.drawable.lock2));
-                layout.setEnabled(false);
-                line.setBackgroundResource(R.drawable.line_locked);
-
-//                remove timer
-                if (rfLockHandlersMap.get(layout) != null) {
-                    rfLockHandlersMap.get(layout).first.removeCallbacksAndMessages(null);
-                }
+                lockLayout(layout);
+                clearLockTimerRunnable(layout);
             } else {
-                //unlocked
-                line.setBackgroundResource(R.drawable.line2);
-                lockClicked = 0;
-                textHeight.setVisibility(View.INVISIBLE);
-                lockBtn.setImageDrawable(getResources().getDrawable(R.drawable.unlock));
-                layout.setEnabled(true);
-                layout.setOnTouchListener(ImageEvaluate.this);
-                //refresh timer
-                if (rfLockHandlersMap.get(layout) != null) {
-                    rfLockHandlersMap.get(layout).first.removeCallbacksAndMessages(null);
-                    Runnable myRunnable = () -> lockLayout(layout);
-                    rfLockHandlersMap.get(layout).first.postDelayed(myRunnable, 2000);
-                    rfLockHandlersMap.put(view, new Pair<>(rfLockHandlersMap.get(layout).first, myRunnable));
-                }
-                else{
-                    Handler handler = new Handler();
-                    Runnable myRunnable = () -> lockLayout(layout);
-                    handler.postDelayed(myRunnable, 2000);
-                    rfLockHandlersMap.put(view, new Pair<>(handler, myRunnable));
-                }
+                unlockLayout(layout);
+                clearLockTimerRunnable(layout);
+                setLockTimerRunnable(layout);
             }
-
         });
         parentLayout.addView(layout);
     }
